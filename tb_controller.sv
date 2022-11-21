307404009c2112be4b2f16cdd88cbf1aa9f2e530
LxsaChn <62408538+LxsaChn@users.noreply.github.com>
2022-11-20 21:45:33 -0800

    Update tb_controller.sv

--- tb_controller.sv
+++ tb_controller.sv
@@ -41,7 +41,7 @@ module tb_controller(output err);
 
   task wait_status(input wait_expect, input string msg);
     assert (waiting_test == wait_expect) begin
-       $diplay("PASS %s: output is %b", msg, waiting_test);
+       $display("PASS %s: output is %b", msg, waiting_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, waiting_test, wait_expect);
@@ -52,7 +52,7 @@ module tb_controller(output err);
 
   task check_reg_sel(input[1:0] reg_sel_expect, input string msg);
     assert (reg_sel_expect == reg_sel_test) begin
-       $diplay("PASS %s: output is %b", msg, reg_sel_test);
+       $display("PASS %s: output is %b", msg, reg_sel_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, reg_sel_test, reg_sel_expect);
@@ -63,7 +63,7 @@ module tb_controller(output err);
 
   task check_wb_sel(input[1:0] wb_sel_expect, input string msg);
     assert (wb_sel_expect == wb_sel_test) begin
-       $diplay("PASS %s: output is %b", msg, wb_sel_test);
+       $display("PASS %s: output is %b", msg, wb_sel_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, wb_sel_test, wb_sel_expect);
@@ -74,7 +74,7 @@ module tb_controller(output err);
 
   task check_w_en(input w_en_expect, input string msg);
     assert (w_en_expect == w_en_test) begin
-       $diplay("PASS %s: output is %b", msg, w_en_expect);
+       $display("PASS %s: output is %b", msg, w_en_expect);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, w_en_test, w_en_expect);
@@ -85,7 +85,7 @@ module tb_controller(output err);
 
   task check_enA(input en_A_expect, input string msg);
     assert (en_A_expect == en_A_test) begin
-       $diplay("PASS %s: output is %b", msg, en_A_test);
+       $display("PASS %s: output is %b", msg, en_A_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, en_A_test, en_A_expect);
@@ -96,7 +96,7 @@ module tb_controller(output err);
 
   task check_enB(input en_B_expect, input string msg);
     assert (en_B_expect == en_B_test) begin
-       $diplay("PASS %s: output is %b", msg, en_B_test);
+       $display("PASS %s: output is %b", msg, en_B_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, en_B_test, en_B_expect);
@@ -107,7 +107,7 @@ module tb_controller(output err);
 
   task check_enC(input en_C_expect, input string msg);
     assert (en_C_expect == en_C_test) begin
-       $diplay("PASS %s: output is %b", msg, en_C_test);
+       $display("PASS %s: output is %b", msg, en_C_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, en_C_test, en_C_expect);
@@ -118,7 +118,7 @@ module tb_controller(output err);
 
   task check_status(input status_expect, input string msg);
     assert (status_expect == en_status_test) begin
-       $diplay("PASS %s: output is %b", msg, en_status_test);
+       $display("PASS %s: output is %b", msg, en_status_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, en_status_test, status_expect);
@@ -129,7 +129,7 @@ module tb_controller(output err);
 
   task check_selA(input sel_A_expect, input string msg);
     assert (sel_A_expect == sel_A_test) begin
-       $diplay("PASS %s: output is %b", msg, sel_A_test);
+       $display("PASS %s: output is %b", msg, sel_A_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, sel_A_test, sel_A_expect);
@@ -140,7 +140,7 @@ module tb_controller(output err);
 
   task check_selB(input sel_B_expect, input string msg);
     assert (sel_B_expect == sel_B_test) begin
-       $diplay("PASS %s: output is %b", msg, sel_B_test);
+       $display("PASS %s: output is %b", msg, sel_B_test);
       num_passes = num_passes + 1;
     end else begin
       $error("FAIL %s: output is %b, expected %b", msg, sel_B_test, sel_B_expect);
@@ -167,20 +167,35 @@ module tb_controller(output err);
 
   initial begin
     assign err_reg = 1'b0;
-    #5;
+    #10;
     reset;
 
     //Op 1 Test
     assign_stuff(3'b110, 2'b10, 2'b10, 1'b0, 1'b0, 1'b0);
     wait_status(1'b1, "IS WAITING AT START");
+    #10;
     start;
     wait_status(1'b0, "NOT WAITING");
     check_enA(1'b0, "A NOT ON WHEN UPDATING");
     check_enB(1'b0, "B NOT ON WHEN UPDATING");
     check_enC(1'b0, "C NOT ON WHEN UPDATING");
     check_w_en(1'b1, "W_EN ON CuZ UPDATING STATUS");
-    check_wb_sel(1'b0, "WB_SEL SAYS COMING FROM C");
-    check_r_addr(3'b101, "WRITING TO 101");
+    check_wb_sel(2'b10, "WB_SEL SAYS COMING FROM SXIMM8");
+
+    //Op 2 test
+    #10;
+    wait_status(1'b1, "IS WAITING AT START");
+
+    assign_stuff(3'b110, 2'b00, 2'b10, 1'b0, 1'b0, 1'b0);
+    #10;
+    start;
+    wait_status(1'b0, "NOT WAITING");
+    check_enA(1'b0, "A NOT ON WHEN NOT UPDATING");
+    check_enB(1'b1, "B ON WHEN UPDATING");
+    check_enC(1'b0, "C NOT ON WHEN UPDATING");
+    check_w_en(1'b0, "W_EN OFF");
+
+
 
     $display("\n\n==== TEST SUMMARY ====");
     $display("  TEST COUNT: %-5d", num_passes + num_fails);
